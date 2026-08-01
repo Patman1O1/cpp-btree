1856ae541fba54555c8be5e9046b6dd696692707
Patrick Thomas <patrickthomas101x@gmail.com>
2026-07-31 23:08:37 -0500

    Remove cpp prefix

--- include/collections/btree.hpp
+++ include/collections/btree.hpp
@@ -1,4 +1,16 @@
-#ifndef CPP_BTREE_HPP
-#define CPP_BTREE_HPP
+#ifndef COLLECTIONS_BTREE_HPP
+#define COLLECTIONS_BTREE_HPP
 
-#endif // #ifndef CPP_BTREE_HPP
+// ISO Includes
+#include <cstddef>
+
+// ISO C++ Includes
+#include <memory>
+
+namespace collections {
+    template<typename T, typename Allocator = std::allocator<T>>
+    class btree {
+    };
+} // namespace collections
+
+#endif // #ifndef COLLECTIONS_BTREE_HPP
